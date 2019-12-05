9f0f07a7530d4522bc7b53849fca01b4cba1ec2b
logotip <logotip123@yahoo.com>
2019-12-05 12:25:33 +0200

    v 1.0

--- tasks.py
+++ tasks.py
@@ -1,12 +1,76 @@
-# Here you implemnt classes to represent tasks, workers and etc.
+"""implement a simple task tracker"""
+
+
+class Worker:
+    """Class worker, need worker name"""
+    def __init__(self, name: str):
+        self._name = name
+
+    def __repr__(self):
+        return self._name
+
+    def __str__(self):
+        return self._name
+
+
+class Task:
+    """fixed payment task"""
+    def __init__(self, task_name: str, payment: int, worker: Worker):
+        self._worker = worker
+        self._task_name = task_name
+        self._payment = payment
+
+    def __repr__(self):
+        return self._task_name
+
+    def __str__(self):
+        return self._task_name
+
+    def get_payment(self):
+        """
+        return payment for task
+        """
+        return self._payment
+
+    def get_worker(self):
+        """
+        return worker who doing this task
+        """
+        return self._worker
+
+class HourlyTask(Task):
+    """hourly payment task"""
+    def __init__(self, task_name: str, payment_per_hour: int, hours_worked: int, worker: Worker):
+        super().__init__(task_name, payment_per_hour, worker)
+        self._hours_worked = hours_worked
+
+    def get_payment(self):
+        """
+        return payment for task
+        """
+        return self._payment * self._hours_worked
+
 
 class Log:
+    """
+    Class for working with logs
+    """
     def __init__(self):
-        pass
+        self._log_list = []
 
     def confirm(self, worker: Worker, task: Task):
-        pass
+        """
+        Confirms the job and logs it.  If the work is done by another employee, return warning.
+        """
+        if worker is not task.get_worker():
+            return f"task: {task} is not {worker} work"
+        self._log_list.append({"worker": worker,
+                               "task": task,
+                               "payment": task.get_payment()})
+        return True
 
     def report(self):
-        ...
-        return ""
+        """
+        return all log list
+        """
+        return self._log_list
--- test_tasks.py
+++ test_tasks.py
@@ -2,6 +2,18 @@ import tasks
 
 
 def test_report():
-    # Data initialization
     return """Bill\t$20
 John]t$30"""
+
+
+def test():
+    worker = tasks.Worker("John")
+    worker2 = tasks.Worker("Uasya")
+    ftask = tasks.Task("go_away", 500, worker2)
+    assert ftask.get_payment() == 500
+    htask = tasks.HourlyTask("go_here", 25, 5, worker)
+    assert htask.get_payment() == 125
+    log = tasks.Log()
+    log.confirm(worker, htask)
+    assert str(log.report()) == "[{'worker': John, 'task': go_here, 'payment': 125}]"
+    assert log.confirm(worker, ftask) == "task: go_away is not John work"
