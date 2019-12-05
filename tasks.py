26a5c9b5f3ff820e80f60336969b17ffda5eede5
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-05 21:27:46 +0200

    1st commit

--- tasks.py
+++ tasks.py
@@ -1,66 +1,107 @@
-
-class Log:
-    def __init__(self):
-        self.payment = {}
-
-    def confirm(self, worker, task):
-        if not task.get_state():
-            task.change_state()
-            worker.change_payment(task.get_cost())
-            self.payment[worker.get_name()] = worker.get_payment()
-        else:
-            print("Rejected. Task is already completed")
-
-    def report(self):
-        print(self.payment)
+"""Module docstring"""
 
 
 class Worker:
-    def __init__(self, name: str):
+    """
+    Class to represent worker
+    """
+    def __init__(self, name):
         self.name = name
-        self.payment = 0
 
     def get_name(self):
+        """
+        Returns:
+            Name of the worker
+        """
         return self.name
 
     def take_task(self, task, log):
-        if isinstance(task, Task) or isinstance(task, HourlyPaymentTask):
-            log.confirm(self, task)
-        else:
-            raise ValueError
-
-    def change_payment(self, value):
-        self.payment += value
-
-    def get_payment(self):
-        return self.payment
+        """
+        Take task and try to confirm it in log
+        Args:
+            task: task that worker try to confirm
+            log: current log
+        """
+        log.confirm(self, task)
 
 
 class Task:
-    def __init__(self, cost, name):
-        self.cost = cost
-        self.name = name
+    """class representing task with fixed payment"""
+    def __init__(self, task_name, payment):
+        self.task_name = task_name
+        self.payment = payment
         self.completed = False
 
-    def get_cost(self):
-        return self.cost
-
-    def get_state(self):
+    def get_status(self):
+        """
+        Returns:
+            Boolean. Status of task: completed or not completed
+        """
         return self.completed
 
-    def change_state(self):
+    def change_status(self):
+        """
+        Returns:
+            Boolean. Changed status of task
+        """
         self.completed = not self.completed
 
-    def get_name(self):
-        return self.name
+    def get_task_name(self):
+        """
+        Returns:
+            Task name
+        """
+        return self.task_name
+
+    def get_payment(self):
+        """
+        Returns:
+            Payment for completing the task
+        """
+        return self.payment
 
 
 class HourlyPaymentTask(Task):
-    def __init__(self, cost, hours, name):
-        super().__init__(cost, name)
-        self.cost = cost
-        self.hours = hours
-        self.name = name
+    """Class to represent task with hourly payment"""
+    def __init__(self, task_name, payment, time_to_complete):
+        super().__init__(task_name, payment)
+        self.time_to_complete = time_to_complete
 
-    def get_cost(self):
-        return self.cost * self.hours
+    def get_payment(self):
+        """
+        Calculate payment for all time of work
+        Returns:
+            Payment for given time of work
+        """
+        return self.payment * self.time_to_complete
+
+
+class Log:
+    """Class to represent log"""
+    def __init__(self):
+        self.payments = {}
+
+    def confirm(self, worker, task):
+        """
+        If task doesnt completed allow worker to complete it.
+        Calculate total payment for worker and add it to log
+        Args:
+            worker:
+            task:
+        """
+        if not task.get_status():
+            task.change_status()
+            if worker.get_name() not in self.payments:
+                self.payments[worker.get_name()] = task.get_payment()
+            else:
+                self.payments[worker.get_name()] += task.get_payment()
+            return "Confirmed"
+
+        return "Rejected. Task already completed"
+
+    def report(self):
+        """
+        Returns:
+            Formed log report
+        """
+        return '\n'.join(f"{name}\t${payment}" for name, payment in self.payments.items())
--- test_tasks.py
+++ test_tasks.py
@@ -1,7 +1,48 @@
 import tasks
 
 
-def test_report():
-    # Data initialization
-    return """Bill\t$20
-John]t$30"""
+def test_report_one():
+    log1 = tasks.Log()
+    worker1 = tasks.Worker("Dima")
+    worker2 = tasks.Worker("John")
+    task1 = tasks.HourlyPaymentTask("Homework", 10, 3)
+    task2 = tasks.Task("Take cup of tea", 5)
+    worker1.take_task(task1, log1)
+    worker2.take_task(task2, log1)
+    assert log1.report() == 'Dima\t$30\nJohn\t$5'
+
+def test_report_two():
+    log1 = tasks.Log()
+    worker1 = tasks.Worker("Dima")
+    worker2 = tasks.Worker("John")
+    task1 = tasks.HourlyPaymentTask("Homework", 10, 3)
+    task2 = tasks.Task("Take cup of tea", 5)
+    worker1.take_task(task1, log1)
+    worker2.take_task(task2, log1)
+    worker1.take_task(task1, log1)
+    assert log1.report() == 'Dima\t$30\nJohn\t$5'
+
+def test_report_three():
+    log1 = tasks.Log()
+    worker1 = tasks.Worker("Dima")
+    worker2 = tasks.Worker("John")
+    task1 = tasks.HourlyPaymentTask("Homework", 10, 3)
+    task2 = tasks.Task("Take cup of tea", 5)
+    task3 = tasks.Task("Web serfing", 25)
+    worker1.take_task(task1, log1)
+    worker2.take_task(task2, log1)
+    worker1.take_task(task3, log1)
+    assert log1.report() == 'Dima\t$55\nJohn\t$5'
+
+def test_report_four():
+    log1 = tasks.Log()
+    worker1 = tasks.Worker("Dima")
+    worker2 = tasks.Worker("John")
+    worker3 = tasks.Worker("Bill")
+    task1 = tasks.HourlyPaymentTask("Homework", 10, 3)
+    task2 = tasks.Task("Take cup of tea", 5)
+    task3 = tasks.Task("Web serfing", 25)
+    worker1.take_task(task1, log1)
+    worker2.take_task(task2, log1)
+    worker3.take_task(task3, log1)
+    assert log1.report() == 'Dima\t$30\nJohn\t$5\nBill\t$25'
