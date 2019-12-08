ae4db22d22005e69d6d1dfb8bb9a1a3ef45d6eed
koreyo <KoretsValentyn@gmail.com>
2019-12-09 00:25:43 +0200

    some fix

--- tasks.py
+++ tasks.py
@@ -1,18 +1,27 @@
+"""There are two kinds of tasks: fixed payment task and hourly payment task.
+ Any task can be assigned to a worker.
+  The worker has a name and can confirm task completion to the log.
+ The log should be able to produce reports with worker's names and payment for completed tasks."""
+
 
 class Worker:
+    """Worker has id, name and can approve task"""
     def __init__(self, worker_id, name):
         self.worker_id = worker_id
         self.name = name
         self.tasks = Register()
 
     def get_name(self):
+        """get worker name"""
         return self.name
 
     def approve(self, log, task):
+        """add task into register list"""
         self.tasks.add_task(self, task)
         log.confirm(self, task)
 
     def get_worker_tasks(self):
+        """get all worker tasks"""
         return self.tasks
 
     def __hash__(self):
@@ -23,10 +32,11 @@ class Worker:
 
 
 class Task:
-    def __init__(self, id_task, name, worker, sum_pay):
+    """task has id, name, sum pay and worker"""
+    def __init__(self, id_task, name, sum_pay):
         self.id_task = id_task
         self.name = name
-        self.worker = worker
+        self.worker = None
         self.sum_pay = sum_pay
 
     def __hash__(self):
@@ -36,45 +46,57 @@ class Task:
         return f"{self.name}: {self.worker.get_name()}, {self.sum_pay} "
 
     def get_name(self):
+        """return task name"""
         return self.name
 
     def get_worker(self):
+        """return worker task"""
         return self.worker
 
 
 class FTask(Task):
-    def __init__(self, id_task, name, worker, sum_pay):
-        super().__init__(id_task, name, worker, sum_pay)
+    """fix price task"""
+    def __init__(self, id_task, name, sum_pay):
+        super().__init__(id_task, name, sum_pay)
 
     def get_sum_pay(self):
+        """return fix price sum"""
         return self.sum_pay
 
 
 class HTask(Task):
-    def __init__(self, id_task, name, worker, sum_pay, hours):
-        super().__init__(id_task, name, worker, sum_pay)
+    """hourly rate task"""
+    def __init__(self, id_task, name, sum_pay, hours):
+        super().__init__(id_task, name, sum_pay)
         self.hours = hours
 
     def get_sum_pay(self):
+        """return sum pay: hours * sum pay"""
         return self.hours * self.sum_pay
 
 
 class Log:
+    """log all tasks with workers"""
     def __init__(self):
         self.logs = Register()
 
     def confirm(self, worker: Worker, task: Task):
+        """confirm task"""
+        task.worker = worker
         self.logs.add_task(worker, task)
 
     def report(self):
+        """str representation"""
         return "\n".join(f"{task.get_name()},{worker.get_name()}: {task.get_sum_pay()}" for task, worker in self.logs)
 
 
 class Register:
+    """container for task and workers. Base on dictionary"""
     def __init__(self):
         self.register_task = {}
 
     def add_task(self, worker, task):
+        """add new task on dictionary"""
         self.register_task[task] = worker
 
     def __repr__(self):
--- test_tasks.py
+++ test_tasks.py
@@ -4,14 +4,16 @@ import tasks
 def test_report():
     w1 = tasks.Worker(1, "bill")
     w2 = tasks.Worker(2, "tom")
-    w3 = tasks.Worker(3, "arnold")
 
-    t1 = tasks.FTask(1, "taskkk1", w1, 34)
-    t2 = tasks.FTask(2, "taskkk2", w1, 10)
-    t3 = tasks.HTask(3, "taskkk3", w3, 5, 10)
+    t1 = tasks.FTask(1, "task1", 34)
+    t2 = tasks.FTask(2, "task2", 10)
+    t3 = tasks.HTask(3, "task3", 5, 10)
 
     l = tasks.Log()
 
     w1.approve(l, t2)
     w2.approve(l, t1)
     w2.approve(l, t3)
+    assert l.report() == "task3,tom: 50\n" \
+                         "task1,tom: 34\n" \
+                         "task2,bill: 10"
