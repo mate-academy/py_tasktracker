314fbd2ed0be58ee23635b64a7d8588456b98a36
Andrii <andrewsotnikov88@gmail.com>
2019-12-10 09:42:39 +0200

    Task tracker

--- tasks.py
+++ tasks.py
@@ -1,12 +1,49 @@
-# Here you implemnt classes to represent tasks, workers and etc.
+"""In this exercise you should implement a simple task tracker. """
+
+
+class Worker:
+    """Worker representation"""
+    def __init__(self, name):
+        self.name = name
+
+    def get_worker_name(self):
+        """Returns worker name"""
+        return self.name
+
+    def report_about_task(self, log, task):
+        """Report about completed task"""
+        log.comfirm(self, task)
+
+
+class Task:
+    """Task representation"""
+    def __init__(self, task, payment, task_type, rate, hours):
+        self.task = task
+        self.payment = payment
+        self.task_type = task_type
+        self.rate = rate
+        self.hours = hours
+
+    def get_task(self):
+        """Returns task"""
+        return self.task
+
+    def get_salary(self):
+        """Returns salary depending on type of task"""
+        if self.task_type == 'Hourly':
+            return self.rate * self.hours
+        return self.payment
+
 
 class Log:
+    """Log represention"""
     def __init__(self):
-        pass
+        self.report_about_task = {}
 
     def confirm(self, worker: Worker, task: Task):
-        pass
+        """Fill info about worker's salary"""
+        self.report_about_task[worker.name] = task.get_salary()
 
     def report(self):
-        ...
-        return ""
+        """Returns salary of every worker"""
+        return '\n'.join("{}\t{}".format(k, v) for k, v in self.report_about_task.items())
